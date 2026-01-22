a4c0724f28aa6bab2aca8b2152891c9483bafcd9
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-01-22 15:32:05 +0530

    established mongodb atlas connection

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from "express";
+//require('dotenv').config({path : '/.env'})
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
 
-const app = express();
+dotenv.config({
+    path : './env'
+})
 
-//USING THE APPROACH ONE : where all the components including database connectivity and express module is written in the same file
-
-( async () => {
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        app.on("error", (error) => {
-            console.log('ERR : ', error);
-            throw error;
-
-        });
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on ${process.env.PORT}`);
-
-
-        })
-        
-    } catch(error) {
-        console.error('ERROR : ', error);
-        throw error
-    }
-    )
-})()
+connectDB()
