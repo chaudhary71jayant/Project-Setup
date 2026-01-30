0567b11460d799026562fdb35bf94942a3544509
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-01-30 21:48:35 +0530

    Written the schemas for the video model and user model and also added a pluginh of mongoose named mongooseAggregationPaginate. Next will be jwt

---
+++ src/models/video.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+    { 
+        videoFiles : {
+            type : String,
+            required : true,
+
+        },
+
+        thumbnail : {
+            type : String,
+            required : true,
+        },
+
+        title : {
+            type : String,
+            required : true,
+        },
+
+        description : {
+            type : String,
+            required : true,
+        },
+
+        title : {
+            type : String,
+            required : true,
+        },
+
+        duration : {
+            type : Number, //will get from cloudinary
+            required : true,
+        },
+
+        views : {
+            type : Number,
+            default : 0,
+        },
+
+        isPublished : {
+            type : Boolean,
+            default : true,
+        },
+
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref : "User",
+        }
+
+    }, 
+    { timestamps : true }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("video", videoSchema)
